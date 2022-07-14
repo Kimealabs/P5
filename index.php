47954a48bef8ec3b17188e4caf3376f149a925a0
Kimea <kimealabs@gmail.com>
2022-07-14 18:47:15 +0200

    update index #1

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
   $loader = require __DIR__ . '/vendor/autoload.php';
-  
+
   $path = $_SERVER["PHP_SELF"];
   if (!defined('PATH')) {
-      define('PATH', 'https://www.kimealabs.xyz/OCR/P5');
+      define('PATH', 'localhost');
   }
 
   $route = new Library\Router();
