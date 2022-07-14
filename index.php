b45743046af32f329ec683402f5821e5d5116617
Kimea <kimealabs@gmail.com>
2022-07-14 19:26:23 +0200

    VSCODE test

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
-<?php
-
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
-  $loader = require __DIR__ . '/vendor/autoload.php';
-
-  $path = $_SERVER["PHP_SELF"];
-  if (!defined('PATH')) {
-      define('PATH', 'localhost');
-  }
-
-  $route = new Library\Router();
-  $ctrl = Library\Factory::follow($route);
+<?php
+
+
+
+error_reporting(E_ALL);
+
+ini_set("display_errors", 1);
+
+  $loader = require __DIR__ . '/vendor/autoload.php';
+
+  $path = $_SERVER["PHP_SELF"];
+
+  if (!defined('PATH')) {
+
+      define('PATH', 'localhost');
+
+  }
+
+
+  $route = new Library\Router();
+
+  $ctrl = Library\Factory::follow($route);
+
