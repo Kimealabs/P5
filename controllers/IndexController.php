a4e08e7a077bc0d918408bf747675263d5bca9d8
Kimea <kimealabs@gmail.com>
2022-07-15 10:11:46 +0200

    beautify

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,13 +1,21 @@
-<?
-  namespace Controllers;
-
-  use Library;
-  use Managers;
-
-  class IndexController {
+<?
 
-    public function default() {
+namespace Controllers;
+
+
+
+use Library;
+
+use Managers;
+
+
+
+class IndexController
+{
+
+
+  public function default()
+  {
     echo "<h1>Hello world !</h1>";
-    }
-
-  }
+  }
+}
--- index.php
+++ index.php
@@ -6,18 +6,16 @@ error_reporting(E_ALL);
 
 ini_set("display_errors", 1);
 
-  $loader = require __DIR__ . '/vendor/autoload.php';
+$loader = require __DIR__ . '/vendor/autoload.php';
 
-  $path = $_SERVER["PHP_SELF"];
+$path = $_SERVER["PHP_SELF"];
 
-  if (!defined('PATH')) {
+if (!defined('PATH')) {
 
-      define('PATH', 'localhost');
+  define('PATH', 'localhost');
+}
 
-  }
 
+$route = new Library\Router();
 
-  $route = new Library\Router();
-
-  $ctrl = Library\Factory::follow($route);
-
+$ctrl = Library\Factory::follow($route);
