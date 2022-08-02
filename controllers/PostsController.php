425941b93c5b8ecc63cacc4f3e9fd02b308d59fe
Kimea <kimealabs@gmail.com>
2022-08-02 14:53:51 +0200

    post controller+view, posts controller + view #11

---
+++ controllers/PostsController.php
@@ -0,0 +1,20 @@
+<?php
+
+  namespace Controllers;
+
+  use Library;
+  use Managers;
+
+  class PostsController extends Library\View {
+
+    public function default() {
+      $this->setData('message', 'Bienvenue à tous<br/>');
+      $postManager = new Managers\PostManager();
+      $userManager = new Managers\UserManager();
+      $posts = $postManager->getAll();
+      $this->setData('userManager', $userManager);
+      $this->setData('posts', $posts);
+      $this->render('posts', 'std');
+    }
+
+  }
