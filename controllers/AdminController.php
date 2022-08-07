d4fd90e1c33fe231ed9ec95cf9afab9df1a526ed
Kimea <kimealabs@gmail.com>
2022-08-07 14:38:06 +0200

    Admin controller + view + template / for user.level > 0 / list blogposts Create Admin Part #6

---
+++ controllers/AdminController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Controllers;
+
+use Library;
+use Managers;
+
+class AdminController extends Library\View
+{
+
+    private function security()
+    {
+        if ($this->session->get('login')) {
+            $userManager = new Managers\UserManager();
+            $user = $userManager->get($this->session->get('login'));
+            if ($user->getLevel() == 0) $this->route->redirect('404');
+            else {
+                $userManager = new Managers\UserManager();
+                $user = $userManager->get($user->getId());
+                $this->setData('user', $user);
+                return true;
+            }
+        } else {
+            $this->route->redirect('404');
+        }
+    }
+
+    public function default()
+    {
+        $this->security();
+        $userManager = new Managers\UserManager();
+        $postManager = new Managers\PostManager();
+        $blogposts = $postManager->getAll();
+        $this->setData('blogposts', $blogposts);
+        $this->setData('userManager', $userManager);
+        $this->render('admin/blogposts', 'admin');
+    }
+
+    public function blogposts()
+    {
+        $this->default();
+    }
+}
---
+++ view/admin/blogposts.view.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <p class="display-6 text-primary mb-0">LISTE DES BLOGPOSTS</p>
+    <p class="mt-0 fs-6">Cliquez sur un Blogpost pour le Modifier/Supprimer</p>
+    <?php
+    foreach ($blogposts as $blogpost) {
+        $user = $userManager->get($blogpost->getUserId());
+    ?>
+        <!-- Post preview-->
+        <div class="post-preview bg-white p-1 mb-4">
+            <a href="admin/blogposts">
+                <h2 class="post-title fs-6"><?= $blogpost->getTitle(); ?></h2>
+                <h3 class="post-subtitle fs-6"><?= $blogpost->getChapo(); ?></h3>
+            </a>
+            <p class="post-meta fs-6">
+                Posté par
+                <b><?= $user->getName(); ?></b>
+                le <?= date("d/m/y", strtotime($blogpost->getCreatedAt())); ?> - modifié le <?= date('d/m/y \à H:i', strtotime($blogpost->getModifiedAt())); ?>
+            </p>
+        </div>
+    <?php
+    }
+    ?>
+</div>
